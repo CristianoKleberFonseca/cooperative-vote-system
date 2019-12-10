f2c1211f01b47b964ea91733bd0fbc85c87beb93
Cristiano Fonseca <ckfonseca@gmail.com>
2019-12-10 00:55:53 -0300

    Inclusao de annotacoes do repositorio.

--- src/main/java/br/com/sicredi/system/repository/AssociateRepository.java
+++ src/main/java/br/com/sicredi/system/repository/AssociateRepository.java
@@ -1,6 +1,13 @@
 package br.com.sicredi.system.repository;
 
-//@Repository
-public interface AssociateRepository /*extends JpaRepository<Associate, Long>*/{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.sicredi.system.model.Associate;
+
+@Repository
+public interface AssociateRepository extends JpaRepository<Associate, Long>{
+	
+	public Associate findByCpf(String cpf);
 
 }
--- src/main/java/br/com/sicredi/system/repository/VotingSessionRepository.java
+++ src/main/java/br/com/sicredi/system/repository/VotingSessionRepository.java
@@ -2,10 +2,13 @@ package br.com.sicredi.system.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import br.com.sicredi.system.model.VotingSession;
 
-//@Repository
-public interface VotingSessionRepository /*extends JpaRepository<VotingSession, Long>*/ {
+@Repository
+public interface VotingSessionRepository extends JpaRepository<VotingSession, Long> {
 	
 	public VotingSession findVotingSessionBySessionTitle(String sessionTitle);
 	
