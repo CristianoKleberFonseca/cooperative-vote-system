58e36d7edb4a054d4b329fb0cebd332a73dfd3cf
Cristiano Fonseca <ckfonseca@gmail.com>
2019-12-10 21:09:49 -0300

    Retirada de import de classe nao utilizado.

--- src/main/java/br/com/sicredi/system/controller/VotingSessionController.java
+++ src/main/java/br/com/sicredi/system/controller/VotingSessionController.java
@@ -1,7 +1,5 @@
 package br.com.sicredi.system.controller;
 
-import java.util.List;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
