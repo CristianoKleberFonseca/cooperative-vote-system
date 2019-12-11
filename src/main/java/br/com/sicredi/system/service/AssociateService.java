35598ae03b886fc40d3921f02c180e7b77842dd6
Cristiano Fonseca <ckfonseca@gmail.com>
2019-12-10 21:11:50 -0300

    Inclusao de tratamento de regras de negocio.

--- src/main/java/br/com/sicredi/system/service/AssociateService.java
+++ src/main/java/br/com/sicredi/system/service/AssociateService.java
@@ -1,40 +1,68 @@
 package br.com.sicredi.system.service;
 
+import java.util.Arrays;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.sicredi.system.adapter.inbound.AssociateAdapterInbound;
+import br.com.sicredi.system.exception.BusinessException;
 import br.com.sicredi.system.model.Associate;
 import br.com.sicredi.system.model.dto.associate.AssociateRequestDto;
-import br.com.sicredi.system.model.dto.associate.AssociateResponsetDto;
+import br.com.sicredi.system.model.dto.error.ErrorDetailDto;
 import br.com.sicredi.system.model.dto.external.ResultHerokuapp;
 import br.com.sicredi.system.model.enumerated.AssociateStatusEnum;
+import br.com.sicredi.system.repository.AssociateRepository;
 
 @Service
+@Transactional
 public class AssociateService {
+	
+	@Autowired
+	private ErrorDetailDto errorDetailDto;
+	
+	@Autowired
+	private MessageService messageService;
 
 	@Autowired
 	private AssociateAdapterInbound associateAdapterInbound;
-//	@Autowired
-//	private AssociateRepository associateRepository;
 	
-	public AssociateResponsetDto insert(AssociateRequestDto associateRequestDto) {
-		AssociateResponsetDto associateResponseDto = null;
+	@Autowired
+	private AssociateRepository associateRepository;
+			
+	public Associate insert(AssociateRequestDto associateRequestDto) {
 		Associate associate = null;
 		
 		if(associateRequestDto.getCpf() == null || "".equals(associateRequestDto.getCpf())) {
-			// TODO: Inserir mensagem de tratamento.
+			this.errorDetailDto = new ErrorDetailDto("AssociateSrvice.insert.field.cpf", this.messageService.getMessage("message.validate.required.associate.cpf"));
+			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto));
 		} else if(associateRequestDto.getName() == null || "".equals(associateRequestDto.getName())) {
-			// TODO: Inserir mensagem de tratamento.
+			this.errorDetailDto = new ErrorDetailDto("AssociateSrvice.insert.field.name", this.messageService.getMessage("message.validate.required.associate.name"));
+			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto)); 
 		}
 		associate = new Associate();
 		BeanUtils.copyProperties(associateRequestDto, associate);
 		associate.setStatus(this.callHerokuapService(associateRequestDto.getCpf()).name());
-		associateResponseDto = new AssociateResponsetDto();
-		BeanUtils.copyProperties(associate, associateResponseDto);
-//		BeanUtils.copyProperties(this.associateRepository.save(associate), associateResponseDto);			
-		return associateResponseDto;
+		associate = this.associateRepository.save(associate);
+		return associate;
+	}
+	
+	public Associate findByCPF(String cpf) {
+		Associate associate = null;
+		
+		if(cpf == null || "".equals(cpf)) {
+			this.errorDetailDto = new ErrorDetailDto("AssociateSrvice.findByCPF.field.cpf", this.messageService.getMessage("message.validate.required.associate.cpf"));
+			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto));
+		}
+		
+		associate = this.associateRepository.findByCpf(cpf);
+		
+		return associate;
 	}
 	
 	private AssociateStatusEnum callHerokuapService(String cpf) {
--- src/main/java/br/com/sicredi/system/service/VotingSessionService.java
+++ src/main/java/br/com/sicredi/system/service/VotingSessionService.java
@@ -1,122 +1,96 @@
 package br.com.sicredi.system.service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import br.com.sicredi.system.model.Vote;
+import br.com.sicredi.system.exception.BusinessException;
 import br.com.sicredi.system.model.VotingSession;
+import br.com.sicredi.system.model.dto.error.ErrorDetailDto;
 import br.com.sicredi.system.model.dto.votingsession.VotingSessionRequestDto;
-import br.com.sicredi.system.model.dto.votingsession.VotingSessionResponseDto;
-import br.com.sicredi.system.model.dto.votingsession.VotingSessionResultDto;
 import br.com.sicredi.system.repository.VotingSessionRepository;
 
 @Service
 @Transactional
 public class VotingSessionService {
-//	@Autowired
+	@Autowired
+	private ErrorDetailDto errorDetailDto;
+	
+	@Autowired
+	private MessageService messageService;
+	
+	@Autowired
 	private VotingSessionRepository votingSessionRepository;
 	
-	private List<VotingSessionResultDto> votingSessionResultDtoList = null;
+	private List<VotingSession> votingSessionList = null;
 	
-	public VotingSessionResponseDto create(VotingSessionRequestDto votingSessionDto) {
-		VotingSessionResponseDto votingSessionResponseDto = null;
-		VotingSession votingSession = null;
+	public VotingSession create(VotingSessionRequestDto votingSessionDto) {
+		VotingSession votingSessionReturn = null;
 
 		if (votingSessionDto.getSessionTitle() == null || "".equals(votingSessionDto.getSessionTitle())) {
-			// TODO: Inserir mensagem de tratamento.
-		} else if (votingSessionDto.getTimeDuration() < 1) {
-			// TODO: Inserir mensagem de tratamento.
+			this.errorDetailDto = new ErrorDetailDto("VotingSessionService.create.field.sessionTitle", this.messageService.getMessage("message.validate.required.votingSession.title"));
+			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto));
+		} else if (votingSessionDto.getTimeDuration() < 0) {
+			this.errorDetailDto = new ErrorDetailDto("VotingSessionService.create.field.timeDuration", this.messageService.getMessage("message.validate.negative.votingSession.timeDuration"));
+			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto));
 		}
 		
-		votingSession = this.votingSessionRepository.findVotingSessionBySessionTitle(votingSessionDto.getSessionTitle());
-		if(votingSession != null) {
-			// TODO: Inserir mensagem de tratamento.
-		} else {
-			votingSession = this.buildVotingSession(votingSessionDto);
-			votingSessionResponseDto = new VotingSessionResponseDto();
-			BeanUtils.copyProperties(votingSession, votingSessionResponseDto);
-//		this.votingSessionRepository.save(votingSession);
+		votingSessionReturn = this.votingSessionRepository.findVotingSessionBySessionTitle(votingSessionDto.getSessionTitle());
+		if(votingSessionReturn != null) {
+			this.errorDetailDto = new ErrorDetailDto("VotingSessionService.create.field.timeDuration", this.messageService.getMessage("message.validate.required.votingSession.timeDuration"));
+			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto));
 		}
+		votingSessionReturn = this.buildVotingSession(votingSessionDto);
+		votingSessionReturn = this.votingSessionRepository.save(votingSessionReturn);
 
-		return votingSessionResponseDto;
+		return votingSessionReturn;
 	}
 
-	public VotingSessionResponseDto findVotingSessionsBySessionTitle(String sessionTitle) {
-		VotingSessionResponseDto votingSessionResponseDto = null;
-		VotingSession votingSession = null;
+	public VotingSession findVotingSessionsBySessionTitle(String sessionTitle) {
+		VotingSession votingSessionReturn = null;
 		
 		if(sessionTitle == null || "".equals(sessionTitle)) {
+			this.errorDetailDto = new ErrorDetailDto("VotingSessionService.findVotingSessionsBySessionTitle.title", this.messageService.getMessage("message.validate.required.votingSession.title"));
 			
+			throw new BusinessException(this.messageService.getMessage("message.title.fiel.required"),  Arrays.asList(this.errorDetailDto));
 		}
-		votingSession = this.votingSessionRepository.findVotingSessionBySessionTitle(sessionTitle);
-		if(votingSession == null) {
-			// TODO: Inserir mensagem de tratamento.
-		}
-		BeanUtils.copyProperties(votingSession, votingSessionResponseDto);
 		
-		return votingSessionResponseDto;
+		votingSessionReturn = this.votingSessionRepository.findVotingSessionBySessionTitle(sessionTitle);
+		
+		return votingSessionReturn;
 	}
 	
 	public List<VotingSession> findVotingSessionsOpened() {
 		List<VotingSession> votingSessionList = null;
 		
-		//TODO: Após construção da camada repository retirar o comentário abaixo e a instanciação do objeto feito na mão.
-//		votingSessionList = this.votingSessionRepository.findVotingSessionByMessageSend(Boolean.FALSE);
-		VotingSession votingSessionMock = new VotingSession();
-		votingSessionMock.setId(1L);
-		votingSessionMock.setMessageSend(Boolean.FALSE);
-		votingSessionMock.setSessionTitle("Votação Teste");
-		votingSessionMock.setVationCreateDate(LocalDateTime.now());
-		
-		Vote vote1 = new Vote();
-		vote1.setId(1L);
-		vote1.setOption(Boolean.TRUE);
-		Vote vote2 = new Vote();
-		vote2.setId(2L);
-		vote2.setOption(Boolean.FALSE);
-		Vote vote3 = new Vote();
-		vote3.setId(3L);
-		vote3.setOption(Boolean.FALSE);
-		votingSessionMock.setVotes(new ArrayList<Vote>());
-		votingSessionMock.getVotes().add(vote1);
-		votingSessionMock.getVotes().add(vote2);
-		votingSessionMock.getVotes().add(vote3);
-		votingSessionList = new ArrayList<VotingSession>();
-		votingSessionList.add(votingSessionMock);
-		
-		if(votingSessionList == null || votingSessionList.size() == 0) {
-			// TODO: Inserir mensagem de tratamento.
-		}
-		
+		votingSessionList = this.votingSessionRepository.findVotingSessionByMessageSend(Boolean.FALSE);
 		votingSessionList.stream().filter(votingSession -> votingSession.getVotationClosedDate().isBefore(LocalDateTime.now()));
 		
 		return votingSessionList;
 	}
 	
-	public List<VotingSessionResultDto> closeVotingSession() {
+	public List<VotingSession> closeVotingSession() {
 		List<VotingSession> votingSessionList = null;
-		this.votingSessionResultDtoList = null;
+		this.votingSessionList = null;
 		
 		votingSessionList = this.findVotingSessionsOpened();
 		if(votingSessionList != null && votingSessionList.size() > 0) {
-			this.votingSessionResultDtoList = new ArrayList<VotingSessionResultDto>();		
 			votingSessionList.forEach(votingSession -> {
 				votingSession.setMessageSend(Boolean.TRUE);
-				VotingSessionResultDto votingSessionResultDto = new VotingSessionResultDto();
-				BeanUtils.copyProperties(votingSession, votingSessionResultDto);
-				this.processResult(votingSession, votingSessionResultDto);
-//			this.votingSessionRepository.save(votingSession);
-				this.votingSessionResultDtoList.add(votingSessionResultDto);
+			this.votingSessionRepository.save(votingSession);
 			});
 		}
-		return this.votingSessionResultDtoList;
+		return this.votingSessionList;
 	}
-
+	
 	private VotingSession buildVotingSession(VotingSessionRequestDto votingSessionRequestDto) {
 		VotingSession votingSession = new VotingSession();
 
@@ -130,23 +104,5 @@ public class VotingSessionService {
 		votingSession.setMessageSend(Boolean.FALSE);
 
 		return votingSession;
-	}
-	
-	private void processResult(VotingSession votingSession, VotingSessionResultDto votingSessionResultDto) {
-		List<Vote> votesList = votingSession.getVotes();
-		votesList.forEach(vote -> {
-			if(vote.isOption()) {
-				votingSessionResultDto.setQtdYes((votingSessionResultDto.getQtdYes() == null ? 1 : votingSessionResultDto.getQtdYes()+1));
-			} else {
-				votingSessionResultDto.setQtdNo((votingSessionResultDto.getQtdNo() == null ? 1 : votingSessionResultDto.getQtdNo()+1));				
-			}
-		});
-		if(votingSessionResultDto.getQtdYes() > votingSessionResultDto.getQtdNo()) {
-			votingSessionResultDto.setResult(VotingSessionResultDto.RESULTADO_SIM_VENCEU);
-		} else if(votingSessionResultDto.getQtdYes() < votingSessionResultDto.getQtdNo()) {
-			votingSessionResultDto.setResult(VotingSessionResultDto.RESULTADO_NAO_VENCEU);
-		} else {
-			votingSessionResultDto.setResult(VotingSessionResultDto.RESULTADO_EMPATE);
-		}
 	}
 }
