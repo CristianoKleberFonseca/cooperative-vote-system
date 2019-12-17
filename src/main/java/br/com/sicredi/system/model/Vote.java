66acc2da093e13d474de53523550a9a34a23ffa8
Cristiano Fonseca <ckfonseca@gmail.com>
2019-12-17 00:41:17 -0300

    Inclusao anotacoes para mapeamento da tabela e entidade.

--- src/main/java/br/com/sicredi/system/model/Vote.java
+++ src/main/java/br/com/sicredi/system/model/Vote.java
@@ -2,8 +2,18 @@ package br.com.sicredi.system.model;
 
 import java.io.Serializable;
 
-//@Entity(name="Vote")
-//@Table(name="vote")
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity(name="Vote")
+@Table(name="vote")
 public class Vote implements Serializable {
 
 	/**
@@ -11,38 +21,39 @@ public class Vote implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long idVote;
 	
-//	@Column(name="cpf")
-	private boolean option;
+	@Column(name="option_vote")
+	private boolean optionVote;
 	
-//	@OneToOne
-//	@JoinColumn(name="associated_id")
-	private Associate associated;
-
-	public Long getId() {
-		return id;
+	@OneToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name="id_associate")
+	private Associate associate;
+	
+	public Long getIdVote() {
+		return idVote;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setIdVote(Long idVote) {
+		this.idVote = idVote;
 	} 
 
-	public boolean isOption() {
-		return option;
+	public boolean isOptionVote() {
+		return optionVote;
 	}
 
-	public void setOption(boolean option) {
-		this.option = option;
+	public void setOptionVote(boolean optionVote) {
+		this.optionVote = optionVote;
 	}
 
-	public Associate getAssociated() {
-		return associated;
+	public Associate getAssociate() {
+		return associate;
 	}
 
-	public void setAssociated(Associate associated) {
-		this.associated = associated;
+	public void setAssociate(Associate associate) {
+		this.associate = associate;
 	}
 }
