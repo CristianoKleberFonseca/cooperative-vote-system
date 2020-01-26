07cf5c842523a3852b57a74cf8e6ef94fb1f076b
Cristiano Fonseca <ckfonseca@gmail.com>
2020-01-26 20:55:53 -0300

    Abertura da cronmask e ajustes no codigo em geral.

--- src/main/java/br/com/sicredi/system/schedule/VotingSessionSchedule.java
+++ src/main/java/br/com/sicredi/system/schedule/VotingSessionSchedule.java
@@ -1,10 +1,12 @@
 package br.com.sicredi.system.schedule;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import br.com.sicredi.system.broker.output.AdapterPublishAssemblyResultBroker;
@@ -16,46 +18,44 @@ import br.com.sicredi.system.service.VotingSessionService;
 @Component
 public class VotingSessionSchedule {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(VotingSessionSchedule.class);
 	@Autowired
-	private VotingSessionService votingSessionService;
-	
+	private VotingSessionService votingSessionService;	
 	@Autowired
 	private AdapterPublishAssemblyResultBroker adapterPublishAssemblyResultBroker; 
 	
-	//	@Scheduled(cron = "0/30 * * * * *")
+	@Scheduled(cron = "0/30 * * * * *")
 	public void closeVotingSession() {
-		List<VotingSession> votingSessionClosedList = null;
-		
-		votingSessionClosedList = this.votingSessionService.closeVotingSession();
-		List<VotingSessionResultDto> votingSessionResultDtoList = new ArrayList<VotingSessionResultDto>();
+		List<VotingSession> votingSessionClosedList = this.votingSessionService.closeVotingSession();
 		
 		if(votingSessionClosedList != null && votingSessionClosedList.size() > 0) {
 			votingSessionClosedList.forEach(votingSession -> {
-				VotingSessionResultDto votingSessionResultDto = new VotingSessionResultDto();
-				BeanUtils.copyProperties(votingSession, votingSessionResultDto);
-//				this.processResult(votingSession.getVotes(), votingSessionResultDto);
-				votingSessionResultDtoList.add(votingSessionResultDto);
+				if(votingSession.getVotes() != null && votingSession.getVotes().size() > 0) {
+					VotingSessionResultDto votingSessionResultDto = new VotingSessionResultDto();
+					BeanUtils.copyProperties(votingSession, votingSessionResultDto);
+					this.processResult(votingSession.getVotes(), votingSessionResultDto);
+					this.adapterPublishAssemblyResultBroker.sendMessageAssemblyResult(votingSessionResultDto);
+				}
 			});
-			this.adapterPublishAssemblyResultBroker.sendMessageAssemblyResult(votingSessionResultDtoList);
 		} else {
-			//TODO: Deverá ser logado a ausência de registro no votação.
+			LOGGER.info("Don't exist voting session register to send.");
 		}
-		
 	}
+	
 	private void processResult(List<Vote> votesList, VotingSessionResultDto votingSessionResultDto) {
 		votesList.forEach(vote -> {
-			if(vote.isOption()) {
+			if(vote.isOptionVote()) {
 				votingSessionResultDto.setQtdYes((votingSessionResultDto.getQtdYes() == null ? 1 : votingSessionResultDto.getQtdYes()+1));
 			} else {
 				votingSessionResultDto.setQtdNo((votingSessionResultDto.getQtdNo() == null ? 1 : votingSessionResultDto.getQtdNo()+1));				
 			}
 		});
 		if(votingSessionResultDto.getQtdYes() > votingSessionResultDto.getQtdNo()) {
-			votingSessionResultDto.setResult(VotingSessionResultDto.RESULTADO_SIM_VENCEU);
+			votingSessionResultDto.setResult(VotingSessionResultDto.RESULT_YES_WINNER);
 		} else if(votingSessionResultDto.getQtdYes() < votingSessionResultDto.getQtdNo()) {
-			votingSessionResultDto.setResult(VotingSessionResultDto.RESULTADO_NAO_VENCEU);
+			votingSessionResultDto.setResult(VotingSessionResultDto.RESULT_NO_WINNER);
 		} else {
-			votingSessionResultDto.setResult(VotingSessionResultDto.RESULTADO_EMPATE);
+			votingSessionResultDto.setResult(VotingSessionResultDto.RESULT_DRAW);
 		}
 	}
 }
